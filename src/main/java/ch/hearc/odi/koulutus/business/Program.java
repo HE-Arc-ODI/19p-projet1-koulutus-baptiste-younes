80c8f5e5f364509bb7db9613bb0aa355e3be4fda
AndresBaptiste <baptiste.andres@he-arc.ch>
2019-04-27 11:16:49 +0200

    Create Program.java
    Add getter/setter

---
+++ src/main/java/ch/hearc/odi/koulutus/business/Program.java
@@ -0,0 +1,88 @@
+package ch.hearc.odi.koulutus.business;
+
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "Program")
+@XmlRootElement(name = "Program")
+public class Program {
+
+  private Long id;
+  private String name;
+  private String richDescription;
+  private String field;
+  private Long price;
+  private List<Course> courses;
+
+
+  public Program() {
+    courses = new ArrayList<>();
+  }
+
+  public Program(String name, String richDescription, String field, Long price) {
+    this();
+    this.name = name;
+    this.richDescription = richDescription;
+    this.field = field;
+    this.price = price;
+  }
+
+  public Program(Long id, String name, String richDescription, String field, Long price) {
+    this(name, richDescription,field,price);
+    this.id = id;
+  }
+
+  public void setCourses(List<Course> courses) {
+    this.courses = courses;
+  }
+
+  @Id
+  @GeneratedValue(generator = "increment")
+  @GenericGenerator(name = "increment", strategy = "increment")
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getRichDescription() {
+    return richDescription;
+  }
+
+  public void setRichDescription(String richDescription) {
+    this.richDescription = richDescription;
+  }
+
+  public String getField() {
+    return field;
+  }
+
+  public void setField(String field) {
+    this.field = field;
+  }
+
+  public Long getPrice() {
+    return price;
+  }
+
+  public void setPrice(Long price) {
+    this.price = price;
+  }
+}
+
